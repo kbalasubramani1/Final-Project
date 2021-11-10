5e6d4e30a2282f298c67171b3cbd058b6079a916
Kavin Bala <kbalasubramani@bsd220.org>
2021-11-10 10:22:46 -0600

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Student on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
