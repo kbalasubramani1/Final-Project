9d93322be51c03c6d2882b0b093c514f5a021c59
Kavin Bala <kbalasubramani@bsd220.org>
2021-11-16 10:37:25 -0600

    Final product again because I forgot to press remote

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -8,6 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputValue = ""
+    @State private var inputUnitValue = 2
+    @State private var outputValue = ""
+    @State private var outputUnitValue = 2
+    let inputUnits = ["meters","kilometers","feet","miles"]
+    let outputUnits = ["meters","kilometers","feet","miles"]
+    var inputUnitAfterConversionToOutput: String {
+        
+        var output = ""
+        var input = Measurement(value: 0, unit: UnitLength.meters)
+        
+        switch inputUnits[inputUnitValue] {
+        case "meters":
+            input = Measurement(value: D, unit: UnitLength.meters)
+        default:
+            <#code#>
+        }
+    }
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
